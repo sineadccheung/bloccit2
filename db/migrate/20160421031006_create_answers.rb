7764004bfead52fab731ad283ff7a7b70bdf5133
Sinead Cheung <sineadccheung@gmail.com>
2016-04-23 10:04:45 -0500

    Assignment 28 completed

--- db/migrate/20160421031006_create_answers.rb
+++ db/migrate/20160421031006_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.references :questions, index: true, foreign_key: true
+      t.references :question, index: true, foreign_key: true
       t.text :body
 
       t.timestamps null: false
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-    let(:questions) { Question.new(title: "New Question Title", body: "New Question Body", resolved: false) }
-    let(:answers) { Answer.new(body: "New Answer Body") }
+    let(:question) { Question.new(title: "New Question Title", body: "New Question Body", resolved: false) }
+    let(:answer) { Answer.new(body: "New Answer Body", question_id: question) }
 
     it "has a body attribute" do
-      expect(answers).to have_attributes(body: "New Answer Body")
+      expect(answer).to have_attributes(body: "New Answer Body")
     end
 end
