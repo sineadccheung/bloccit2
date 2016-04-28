d5a94d71d64d77ac75b4b21e7494298535b7ab65
Sinead Cheung <sineadccheung@gmail.com>
2016-04-27 21:55:17 -0500

    Assignment 34 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424221254) do
+ActiveRecord::Schema.define(version: 20160427131902) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -40,13 +40,16 @@ ActiveRecord::Schema.define(version: 20160424221254) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-  create_table "questions", force: :cascade do |t|
+  create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "resolved"
+    t.integer  "price"
+    t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
