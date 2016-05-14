95af7b746d0006c54aa215f64c7ed61d3408dc3e
Sinead Cheung <sineadccheung@gmail.com>
2016-05-13 20:16:57 -0500

    Checkpoint 43 completed

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+     post '/down-vote' => 'votes#down_vote', as: :down_vote
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
