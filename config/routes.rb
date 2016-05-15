67c1f0fe49ceadc21097c354332eac47d5d6480d
Sinead Cheung <sineadccheung@gmail.com>
2016-05-15 15:57:58 -0500

    checkpoint 48 done

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,12 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  namespace :api do
+     namespace :v1 do
+       resources :users, only: [:index, :show]
+       resources :topics, only: [:index, :show]
+     end
+   end
+
 
 end
