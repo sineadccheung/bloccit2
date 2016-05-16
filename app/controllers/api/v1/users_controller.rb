4d8d912e28efb408247937f8b510a94ee80b0182
Sinead Cheung <sineadccheung@gmail.com>
2016-05-15 22:31:09 -0500

    checkpoint 45 done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,4 +13,36 @@ class Api::V1::UsersController < Api::V1::BaseController
 # #18
     render json: users, status: 200
   end
+
+  def update
+    user = User.find(params[:id])
+
+    # #9
+        if user.update_attributes(user_params)
+          render json: user, status: 200
+        else
+          render json: { error: "User update failed", status: 400 }, status: 400
+        end
+  end
+
+  def create
+
+    user = User.new(user_params)
+
+ # #14
+     if user.valid?
+       user.save!
+       render json: user, status: 201
+     else
+       render json: { error: "User is invalid", status: 400 }, status: 400
+     end
+
+  end
+
+  private
+ # #10
+   def user_params
+     params.require(:user).permit(:name, :email, :password, :role)
+   end
+
 end
